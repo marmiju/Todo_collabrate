4ccb521c0d3ba571f1cb34e7bd2169f282c03333
marmiju <mar.miju.dev@gmail.com>
2025-04-25 18:03:15 +0600

    onchenge

--- src/Components/Inputtfield/InputField.jsx
+++ src/Components/Inputtfield/InputField.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Inputtfield/input.css'
 
 export const InputField = () => {
+    const [value, setValue] = useState('')
+
+    function handlechenge (e){
+         val = e.target.value
+         console.log(val)
+
+    }
+ 
   return (
     <div className='indiv'>
-        <input className='inputbox' type="text"/>
+        <input onChange={handlechenge} className='inputbox' type="text"/>
         <button>Add New</button>
     </div>
   )
